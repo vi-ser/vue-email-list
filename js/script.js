fd5b41e7a69bd41c14a918c8398f531c6619c8f7
vi_ser <vinceulisse@gmail.com>
2024-02-21 15:18:55 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ createApp({
 
             emails: [],
 
+            listNumber: 0,
+
 
         }
     },
@@ -18,17 +20,24 @@ createApp({
 
     // al caricamento della pagina:
     mounted() {
+        // memorizzo le richieste in un array
+        const requests = [];
 
+        // memorizzo le chiamate API nell'array 'requests'
         for (let i = 0; i < 10; i++) {
-
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-
-
-                this.randomEmail = response.data.response;
-                this.emails.push(this.randomEmail)
-            });
+            requests.push(
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    this.emails.push(response.data.response);
+                })
+            );
         }
-        // console.log(this.emails)
+
+        // attendo il completamento di tutte le promesse (chiesto aiuto a ChatGpt)
+        Promise.all(requests).then(() => {
+
+            this.listNumber = this.emails.length;
+
+        })
     }
 
 }).mount('#app');
