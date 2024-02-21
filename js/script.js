978007a79d0c18027b463ba28b9b69cc5a18f76e
vi_ser <vinceulisse@gmail.com>
2024-02-21 14:13:19 +0100

    generazione 1 email casuale

--- js/script.js
+++ js/script.js
@@ -11,4 +11,16 @@ createApp({
 
         }
     },
+
+
+    // al caricamento della pagina:
+    mounted() {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+
+
+            this.email = response.data.response;
+        });
+    }
+
 }).mount('#app');
